7a5e62c9a522fc62aa91a30364999811cd67d9a6
jatbio <jattoaabiodun@yahoo.com>
2022-09-08 08:24:19 -0700

    Code pushing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[65];
+	strcpy(str1, "with proper grammar, but the outcome is a piece of art,\n");
+	puts(str1);
+	return (0);
+
+}
