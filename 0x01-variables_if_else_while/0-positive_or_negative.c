f192af251e596378fc7d1073bfcb26f245634fc0
jatbio <jattoaabiodun@yahoo.com>
2022-09-09 06:19:36 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - to print if statement is satisfactory
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	int n;
