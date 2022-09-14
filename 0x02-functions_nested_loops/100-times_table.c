fa3e90452ecb5cf57a1016f220c237e02d9cdaf1
jatbio <jattoaabiodun@yahoo.com>
2022-09-13 21:35:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,28 +17,28 @@ void print_times_table(int n)
 
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 
 				prod = num * mult;
 
 				if (prod <= 99)
-					_putchar(' ');
+					putchar(' ');
 				if (prod <= 9)
-					_putchar(' ');
+					putchar(' ');
 
 				if (prod >= 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					putchar((prod / 100) + '0');
+					putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((prod / 10) + '0');
+					putchar((prod / 10) + '0');
 				}
-				_putchar((prod % 10) + '0');
+				putchar((prod % 10) + '0');
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
